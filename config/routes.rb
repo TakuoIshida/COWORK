bb3682dc2ef0c78ff9bbc6ef7be1fd6946e01236
ishida <ec2-user@ip-172-31-38-70.ap-northeast-1.compute.internal>
2019-11-25 15:06:42 +0000

    config_dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  resources :pages
-  devise_for :users
+  root 'pages#index'
+
+  devise_for :users, at: 'users', controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
+  }
   resources :work_places
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #   resources :comments, only:[:index,:new, :create]
+  # end
+  get 'workplaces/find'
+  post 'workplaces/find'
+
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
